32a7c88418978d3fe4ee72280113fccaafb2d7c0
majinliang123 <majinliang@huaxiafinance.com>
2016-09-27 22:48:58 +0800

    解析xml

--- src/main/java/com/spring/ioc/resource/XmlBeanDefinitionReader.java
+++ src/main/java/com/spring/ioc/resource/XmlBeanDefinitionReader.java
@@ -16,35 +16,61 @@ import java.util.Map;
  */
 public class XmlBeanDefinitionReader {
 
-    Map<String,BeanDefinition> map = new HashMap<String,BeanDefinition>();
+    Map<String,BeanDefinition> beanMap = new HashMap<String,BeanDefinition>();
 
     public void loadBeanDefinitions(String xmlName) throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
-        Document document = db.parse("");
-
+        String path = this.getClass().getClassLoader().getResource("").getPath();
+        Document document = db.parse(path  + xmlName);
         //根节点
         Node rootNode = document.getElementsByTagName("beans").item(0);
-
         //bean节点
         NodeList nodeList = rootNode.getChildNodes();
         for(int i=0;i<nodeList.getLength();i++){
             Node node = nodeList.item(i);
+            if(node.getNodeType()!=1) {
+                continue;
+            }
             NamedNodeMap namedNodeMap = node.getAttributes();
             Node idNode = namedNodeMap.getNamedItem("id");
             Node classNode = namedNodeMap.getNamedItem("class");
             BeanDefinition beanDefinition = new BeanDefinition();
             beanDefinition.setBeanClassName(classNode.getNodeValue());
+            setPropertyValues(node,beanDefinition);
+            beanMap.put(idNode.getNodeValue(),beanDefinition);
+        }
+    }
 
+    private void setPropertyValues(Node node,BeanDefinition beanDefinition) {
+        PropertyValues propertyValues = new PropertyValues();
+        NodeList nodeList = node.getChildNodes();
+        for(int i=0;i<nodeList.getLength();i++) {
+            Node childNode = nodeList.item(i);
+            if(childNode.getNodeType()!=1) {
+                continue;
+            }
+            NamedNodeMap namedNodeMap = childNode.getAttributes();
+            String name =  namedNodeMap.getNamedItem("name").getNodeValue();
+            String value = namedNodeMap.getNamedItem("value").getNodeValue() ;
+            PropertyValue propertyValue = new PropertyValue(name,value);
+            propertyValues.addPropertyValue(propertyValue);
+            beanDefinition.setPropertyValues(propertyValues);
+        }
+    }
+
+    public static void main(String args []) {
+
+        XmlBeanDefinitionReader xdr = new XmlBeanDefinitionReader();
+        try {
+            xdr.loadBeanDefinitions("beans.xml");
+            Map map = xdr.beanMap;
+            System.out.println(map.size());
+        } catch(Exception e) {
+            e.printStackTrace();
         }
 
 
 
-    }
-
-    private PropertyValues getPropertyValues(Node node,BeanDefinition beanDefinition) {
-        PropertyValues propertyValues = beanDefinition.getPropertyValues();
-        NodeList nodeList = node.getChildNodes();
-        return null;
     }
 }
